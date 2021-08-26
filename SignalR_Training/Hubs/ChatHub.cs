0106d86a1aa7ebb139a2a0f3643ab2031eb56ee2
Ratte222 <Artur21111999.o@gmail.com>
2021-08-26 16:05:21 +0300

    Добавьте файлы проекта.

---
+++ SignalR_Training/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SignalR_Training.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{user} ({Context.ConnectionId})", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync("Notify", $"{Context.ConnectionId} entered the chat");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} escaped the chat");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SignalR_Training/wwwroot/js/Chat.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Information).withUrl("/chatHub").build();
+
+connection.serverTimeoutInMilliseconds = 1000 * 60 * 10;
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    console.log(`ReceiveMessage user: ${user} message: ${message}`);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.on('Notify', function (message) {
+
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    console.log(`Notify message: ${message}`);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = message;
+    
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
